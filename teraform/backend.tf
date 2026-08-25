cc386c9ea3e7a660fa27c7ba1be350b3a81c08b1
rajapandi29 <rp6321613@gmail.com>
2026-08-25 17:31:13 +0530

    new

--- teraform/backend.tf
+++ teraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "my-terraformstate-18-8"
-    key    = "Mug/buckets/my-terraformstate-18-8"
+    key    = "Mug/my-terraformstate-18-8"
     region = "us-east-1"
     encrypt = true
 
