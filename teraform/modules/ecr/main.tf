6f41e23584d61084ce3ebb4f34f3242477c63cf6
Rajapandi29 <rp6321613@gmail.com>
2026-08-25 14:30:25 +0530

    Parameterize ECR repository name with variables

--- teraform/modules/ecr/main.tf
+++ teraform/modules/ecr/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "app" {
-    name        = "todo-app"
+    name        = "${var.project_name}-${var.environment}"
     image_tag_mutability = "MUTABLE"
 
     image_scanning_configuration {
