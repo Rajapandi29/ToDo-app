1f345d01ce4bd08f58cc16d748c7285b5e8d3b79
rajapandi29 <rp6321613@gmail.com>
2026-08-31 20:52:40 +0530

    new

--- teraform/main.tf
+++ teraform/main.tf
@@ -1,75 +1,183 @@
 module "vpc" {
-      source = "s3::https://s3-us-east-1.amazonaws.com/terraform-module-23-8/vpc"
+  source = "s3::https://s3-us-east-1.amazonaws.com/terraform-module-23-8/vpc"
 
-      project_name  = var.project_name
-      environment   = var.environment
-      vpc_cidr      = var.vpc_cidr
-      public_subnet_cidrs = var.public_subnet_cidrs
-      private_subnet_cidrs = var.private_subnet_cidrs
+  project_name          = var.project_name
+  environment           = var.environment
+  vpc_cidr              = var.vpc_cidr
+  public_subnet_cidrs   = var.public_subnet_cidrs
+  private_subnet_cidrs  = var.private_subnet_cidrs
 }
-
 resource "aws_sns_topic" "alerts" {
-    name = "${var.project_name}-${var.environment}-alerts"
+  name = "${var.project_name}-${var.environment}-alerts"
 }
-resource "aws_sns_topic_subscription" "email_alart" {
-    topic_arn = aws_sns_topic.alerts.arn
-    protocol  = "email"
-    endpoint  = var.alerts_email
+
+resource "aws_sns_topic_subscription" "email_alert" {
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "email"
+  endpoint  = var.alerts_email
 }
+
 resource "aws_sns_topic_subscription" "sms_alert" {
-    count      = var.alert_phone != "" ? 1 : 0
-    topic_arn  = aws_sns_topic.alerts.arn
-    protocol   = "sms"
-    endpoint   = var.alert_phone
+  count     = var.alert_phone != "" ? 1 : 0
+  topic_arn = aws_sns_topic.alerts.arn
+  protocol  = "sms"
+  endpoint  = var.alert_phone
 }
-resource "aws_sns_topic_policy" "allow_cloudwatch_eventbridge_publish" {
-    arn = aws_sns_topic.alerts.arn
 
-    policy = jsonencode ({
-        Version = "2012-10-17"
-        Statement = [{
-            Sid       = "AllowPublish"
-            Effect    = "Allow"
-            Principal = {Service = ["events.amazonaws.com", "cloudwatch.amazonaws.com"]}
-            Action    = "SNS:Publish"
-            Resource  = aws_sns_topic.alerts.arn
+resource "aws_sns_topic_policy" "allow_cloudwatch_publish" {
+  arn = aws_sns_topic.alerts.arn
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "AllowPublish"
+        Effect = "Allow"
+
+        Principal = {
+          Service = [
+            "events.amazonaws.com",
+            "cloudwatch.amazonaws.com"
+          ]
         }
-        ]
-    })
 
+        Action   = "SNS:Publish"
+        Resource = aws_sns_topic.alerts.arn
+      }
+    ]
+  })
 }
 
 module "alb" {
-    source = "s3::https://s3-us-east-1.amazonaws.com/terraform-module-23-8/alb"
+  source = "s3::https://s3-us-east-1.amazonaws.com/terraform-module-23-8/alb"
 
-    project_name        = var.project_name
-    environment         = var.environment
-    vpc_id              = module.vpc.vpc_id
-    public_subnet_ids   = module.vpc.public_subnet_ids
-    container_port      = var.container_port
-    alert_sns_topic_arn = aws_sns_topic.alerts.arn
+  project_name        = var.project_name
+  environment         = var.environment
+  vpc_id              = module.vpc.vpc_id
+  public_subnet_ids   = module.vpc.public_subnet_ids
+  container_port      = var.container_port
+  alert_sns_topic_arn = aws_sns_topic.alerts.arn
 }
- module "ecr" {
-    source = "./modules/ecr"
 
-    project_name = var.project_name
-    environment  = var.environment
- }
 
- module "ecs" {
-    source = "./modules/ecs"
+module "ecr" {
+  source = "./modules/ecr"
 
-    project_name   = var.project_name
-    environment    = var.environment
-    aws_region     = var.aws_region
-    vpc_id         = module.vpc.vpc_id
-    private_subnet_ids = module.vpc.private_subnet_ids
-    alb_sg_id          = module.alb.alb_sg_id
-    container_image    = "${module.ecr.ecr_repository_url}:latest"
-    container_port     = var.container_port
-    cpu                = var.cpu
-    memory             = var.memory
-    desired_count      = var.desired_count
-    target_group_arn   = module.alb.target_group_arn
-    alert_sns_topic_arn = aws_sns_topic.alerts.arn    
- }
+  project_name = var.project_name
+  environment  = var.environment
+}
+
+module "ecs" {
+  source = "./modules/ecs"
+
+  project_name       = var.project_name
+  environment        = var.environment
+  aws_region         = var.aws_region
+
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+
+  alb_sg_id          = module.alb.alb_sg_id
+
+  container_image    = "${module.ecr.ecr_repository_url}:latest"
+  container_port     = var.container_port
+
+  cpu                = var.cpu
+  memory             = var.memory
+  desired_count      = var.desired_count
+
+  target_group_arn   = module.alb.target_group_arn
+
+  alert_sns_topic_arn = aws_sns_topic.alerts.arn
+}
+
+module "sample_ecr" {
+  source = "./modules/ecr"
+
+  project_name = var.sample_project_name
+  environment  = var.environment
+}
+data "aws_lb" "existing" {
+  name = var.alb_name
+
+  depends_on = [
+    module.alb
+  ]
+}
+
+data "aws_lb_listener" "http" {
+  load_balancer_arn = data.aws_lb.existing.arn
+  port              = 80
+
+  depends_on = [
+    module.alb
+  ]
+}
+
+resource "aws_lb_target_group" "sample" {
+  name        = "${var.sample_project_name}-${var.environment}-tg"
+  port        = var.sample_container_port
+  protocol    = "HTTP"
+  vpc_id      = module.vpc.vpc_id
+  target_type = "ip"
+
+  health_check {
+    path                = var.sample_health_check_path
+    protocol            = "HTTP"
+    port                = "traffic-port"
+
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval             = 30
+  }
+
+  tags = {
+    Name        = "${var.sample_project_name}-${var.environment}-tg"
+    Application = var.sample_project_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_listener_rule" "sample" {
+  listener_arn = data.aws_lb_listener.http.arn
+  priority     = var.sample_listener_priority
+
+  condition {
+    path_pattern {
+      values = [
+        "/sample",
+        "/sample/*"
+      ]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.sample.arn
+  }
+}
+module "sample_ecs" {
+  source = "./modules/ecs"
+
+  project_name       = var.sample_project_name
+  environment        = var.environment
+  aws_region         = var.aws_region
+
+  vpc_id             = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+
+  alb_sg_id          = module.alb.alb_sg_id
+
+  container_image    = "${module.sample_ecr.ecr_repository_url}:latest"
+  container_port     = var.sample_container_port
+
+  cpu                = var.sample_cpu
+  memory             = var.sample_memory
+  desired_count      = var.sample_desired_count
+
+  target_group_arn   = aws_lb_target_group.sample.arn
+
+  alert_sns_topic_arn = aws_sns_topic.alerts.arn
+}
--- teraform/variables.tf
+++ teraform/variables.tf
@@ -1,47 +1,110 @@
 variable "project_name" {
-    type    = string
-    default = "sample"
+  type        = string
+  description = "Todo application project name"
 }
+
 variable "environment" {
-    type    = string
-    default = "dev"
+  type        = string
+  description = "Environment name"
 }
+
 variable "aws_region" {
-    type     = string
-    default  = "us-east-1" 
+  type        = string
+  description = "AWS region"
 }
+
+
+
 variable "vpc_cidr" {
-    type    = string
-    default = "10.0.0.0/16"
+  type        = string
+  description = "VPC CIDR block"
 }
+
 variable "public_subnet_cidrs" {
-    type    = list(string)
-    default = ["10.0.1.0/24", "10.0.12.0/24"] 
+  type        = list(string)
+  description = "Public subnet CIDR blocks"
 }
+
 variable "private_subnet_cidrs" {
-    type  = list(string)
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
+  type        = list(string)
+  description = "Private subnet CIDR blocks"
 }
+
+
+
 variable "container_port" {
-    type    = number
-    default = 8080
+  type        = number
+  description = "Todo application container port"
 }
+
 variable "cpu" {
-    type    = number
-    default = 256
+  type        = number
+  description = "Todo ECS CPU"
 }
+
 variable "memory" {
-    type    = number
-    default = 512
+  type        = number
+  description = "Todo ECS memory"
 }
+
 variable "desired_count" {
-    type    =  number
-    default = 1
+  type        = number
+  description = "Todo ECS desired task count"
 }
+
+
 variable "alerts_email" {
-    type   = string
+  type        = string
+  description = "SNS alert email address"
 }
+
 variable "alert_phone" {
-    type    = string
-    default = ""
+  type        = string
+  description = "SNS alert phone number"
+
+  default = ""
+}
+
+
+variable "alb_name" {
+  type        = string
+  description = "Existing ALB name created by the S3 ALB module"
+}
+
+
+variable "sample_project_name" {
+  type        = string
+  description = "Sample application project name"
+}
+
+variable "sample_container_port" {
+  type        = number
+  description = "Sample application container port"
+}
+
+variable "sample_cpu" {
+  type        = number
+  description = "Sample ECS CPU"
+}
+
+variable "sample_memory" {
+  type        = number
+  description = "Sample ECS memory"
+}
+
+variable "sample_desired_count" {
+  type        = number
+  description = "Sample ECS desired task count"
+}
+
+
+
+variable "sample_health_check_path" {
+  type        = string
+  description = "Sample application health check path"
+}
+
+variable "sample_listener_priority" {
+  type        = number
+  description = "ALB listener rule priority for Sample application"
 }
